cc79ea450f5183521d65c46c47d39a295d2658ce
Tyson Fernstrom <tyson.fernstrom@gmail.com>
2023-10-29 10:36:41 -0500

    lab 5 remove console.logs

--- js/app.js
+++ js/app.js
@@ -9,10 +9,8 @@ while (!userName){
 alert(`Welcome to my site ${userName}! Let's play a guessing game, please answer yes/no OR y/n`);
 
 let questionOneGuess = prompt('Am I 30 years old?').toLowerCase();
-console.log('Question One');
 
 if (questionOneGuess === 'y' || questionOneGuess === 'yes'){
-  //console.log('You are correct!');
   alert('You are correct!');
   correctAnswerTotal++;
 } else if (questionOneGuess === 'n' || questionOneGuess === 'no'){
@@ -22,7 +20,6 @@ if (questionOneGuess === 'y' || questionOneGuess === 'yes'){
 let questionTwoGuess = prompt('Am I married?').toLowerCase();
 
 if (questionTwoGuess === 'y' || questionTwoGuess === 'yes'){
-  //console.log('You are correct!');
   alert('You are correct!');
   correctAnswerTotal++;
 }
@@ -33,7 +30,6 @@ else if (questionTwoGuess === 'n' || questionTwoGuess === 'no'){
 let questionThreeGuess = prompt('Did I grow up in Texas?').toLowerCase();
 
 if (questionThreeGuess === 'n' || questionThreeGuess === 'no'){
-  //console.log('You are correct!');
   alert('You are correct!');
   correctAnswerTotal++;
 }
@@ -44,7 +40,6 @@ else if (questionThreeGuess === 'y' || questionThreeGuess === 'yes'){
 let questionFourGuess = prompt('Am I interested in full stack, cloud engineering, and machine learning?').toLowerCase();
 
 if (questionFourGuess === 'y' || questionFourGuess === 'yes'){
-  //console.log('You are correct!');
   alert('You are correct!');
   correctAnswerTotal++;
 }
@@ -55,7 +50,6 @@ else if (questionFourGuess === 'n' || questionFourGuess === 'no'){
 let questionFiveGuess = prompt('Is my highest level of education a bachelors degree?').toLowerCase();
 
 if (questionFiveGuess === 'n' || questionFiveGuess === 'no'){
-  //console.log('You are correct!');
   alert('You are correct!');
   correctAnswerTotal++;
 }
