15c3a7f057af7a6b559cf06f10fe999da2adc6ef
Tyson Fernstrom <tyson.fernstrom@gmail.com>
2023-11-04 13:41:46 -0500

    lab 4 functions add

--- js/app.js
+++ js/app.js
@@ -1,123 +1,143 @@
 'use strict';
 
 let correctAnswerTotal = 0;
-
-let userName= prompt('What is your name?');
-while (!userName){
-  userName = prompt('Enter your name to continue.');
-}
-alert(`Welcome to my site ${userName}! Let's play a guessing game, please answer yes/no OR y/n`);
-
-let questionOneGuess = prompt('Am I 30 years old?').toLowerCase();
-
-if (questionOneGuess === 'y' || questionOneGuess === 'yes'){
-  alert('You are correct!');
-  correctAnswerTotal++;
-} else if (questionOneGuess === 'n' || questionOneGuess === 'no'){
-  alert('Sorry, you are wrong.');
-}
-
-let questionTwoGuess = prompt('Am I married?').toLowerCase();
-
-if (questionTwoGuess === 'y' || questionTwoGuess === 'yes'){
-  alert('You are correct!');
-  correctAnswerTotal++;
-}
-else if (questionTwoGuess === 'n' || questionTwoGuess === 'no'){
-  alert('Sorry, you are wrong.');
-}
-
-let questionThreeGuess = prompt('Did I grow up in Texas?').toLowerCase();
-
-if (questionThreeGuess === 'n' || questionThreeGuess === 'no'){
-  alert('You are correct!');
-  correctAnswerTotal++;
-}
-else if (questionThreeGuess === 'y' || questionThreeGuess === 'yes'){
-  alert('Sorry, you are wrong, I grew up in Oregon.');
-}
-
-let questionFourGuess = prompt('Am I interested in full stack, cloud engineering, and machine learning?').toLowerCase();
-
-if (questionFourGuess === 'y' || questionFourGuess === 'yes'){
-  alert('You are correct!');
-  correctAnswerTotal++;
-}
-else if (questionFourGuess === 'n' || questionFourGuess === 'no'){
-  alert('Sorry, you are wrong');
-}
-
-let questionFiveGuess = prompt('Is my highest level of education a bachelors degree?').toLowerCase();
-
-if (questionFiveGuess === 'n' || questionFiveGuess === 'no'){
-  alert('You are correct!');
-  correctAnswerTotal++;
-}
-else if (questionFiveGuess === 'y' || questionFiveGuess === 'yes'){
-  alert('Sorry, you are wrong');
-}
-
-let favoriteNumber = 7;
-let questionSixAllowedGuesses = 4;
-let questionSixRemainingGuesses = questionSixAllowedGuesses;
-
-while (questionSixRemainingGuesses > 0) {
-  let questionSixGuess = parseInt(prompt(`Guess your favorite number (between 1 and 10). You have ${questionSixRemainingGuesses} attempts left.`));
-
-  if (questionSixGuess === favoriteNumber) {
-    alert(`Congratulations! ${favoriteNumber} is your favorite number!`);
-    correctAnswerTotal++;
-    break;
-  } else if (questionSixGuess< favoriteNumber) {
-    alert('Your guess is too low. Try again.');
-  } else if (questionSixGuess > favoriteNumber) {
-    alert('Your guess is too high. Try again.');
+function questionName() {
+  let userName= prompt('What is your name?');
+  while (!userName){
+    userName = prompt('Enter your name to continue.');
   }
+  alert(`Welcome to my site ${userName}! Let's play a guessing game, please answer yes/no OR y/n`);
 
-  questionSixRemainingGuesses--;
+  let questionOneGuess = prompt('Am I 30 years old?').toLowerCase();
+
+  if (questionOneGuess === 'y' || questionOneGuess === 'yes'){
+    alert('You are correct!');
+    correctAnswerTotal++;
+  } else if (questionOneGuess === 'n' || questionOneGuess === 'no'){
+    alert('Sorry, you are wrong.');
+  }
 }
+questionName();
 
-if (questionSixRemainingGuesses === 0) {
-  alert(`Sorry, you've used all your attempts. The correct answer is ${favoriteNumber}.`);
+function questionMarried() {
+  let questionTwoGuess = prompt('Am I married?').toLowerCase();
+
+  if (questionTwoGuess === 'y' || questionTwoGuess === 'yes'){
+    alert('You are correct!');
+    correctAnswerTotal++;
+  }
+  else if (questionTwoGuess === 'n' || questionTwoGuess === 'no'){
+    alert('Sorry, you are wrong.');
+  }
 }
+questionMarried();
 
-function questionSevenGuess() {
-  let questionSevenAnswers = ['dog','rat','bunny'];
-  let questionSevenAllowedGuesses = 6;
-  let questionSevenRemainingGuesses = questionSevenAllowedGuesses;
+function questionWhereRaised() {
+  let questionThreeGuess = prompt('Did I grow up in Texas?').toLowerCase();
 
-  for(let i=0; i < questionSevenAllowedGuesses; i++){
-    let questionSevenGuess = prompt(`Can you guess what animals I had as a kid? You have ${questionSevenRemainingGuesses} attempts left!`);
+  if (questionThreeGuess === 'n' || questionThreeGuess === 'no'){
+    alert('You are correct!');
+    correctAnswerTotal++;
+  }
+  else if (questionThreeGuess === 'y' || questionThreeGuess === 'yes'){
+    alert('Sorry, you are wrong, I grew up in Oregon.');
+  }
+}
+questionWhereRaised();
 
-    if (questionSevenAnswers.includes(questionSevenGuess)) {
-      let index = questionSevenAnswers.indexOf(questionSevenGuess);
+function questionCodeInterest() {
+  let questionFourGuess = prompt('Am I interested in full stack, cloud engineering, and machine learning?').toLowerCase();
 
-      let remainingAnswersString = '';
+  if (questionFourGuess === 'y' || questionFourGuess === 'yes'){
+    alert('You are correct!');
+    correctAnswerTotal++;
+  }
+  else if (questionFourGuess === 'n' || questionFourGuess === 'no'){
+    alert('Sorry, you are wrong');
+  }
+}
+questionCodeInterest();
 
-      for (let i = 0; i < questionSevenAnswers.length; i++) {
-        if (i !== index) {
-          remainingAnswersString += questionSevenAnswers[i];
-          if (i < questionSevenAnswers.length - 2) {
-            remainingAnswersString += ' , ';
-          } else if (i === questionSevenAnswers.length - 2) {
-            remainingAnswersString += ' and ';
-          }
-        }
-      }
-      alert(`You are correct! ${questionSevenGuess} is one of the correct answers! The other correct answers are: ${remainingAnswersString}.`);
+function questionEducation() {
+  let questionFiveGuess = prompt('Is my highest level of education a bachelors degree?').toLowerCase();
 
+  if (questionFiveGuess === 'n' || questionFiveGuess === 'no'){
+    alert('You are correct!');
+    correctAnswerTotal++;
+  }
+  else if (questionFiveGuess === 'y' || questionFiveGuess === 'yes'){
+    alert('Sorry, you are wrong');
+  }
+}
+questionEducation();
+
+function questionFavoriteNumber() {
+  let favoriteNumber = 7;
+  let questionSixAllowedGuesses = 4;
+  let questionSixRemainingGuesses = questionSixAllowedGuesses;
+
+  while (questionSixRemainingGuesses > 0) {
+    let questionSixGuess = parseInt(prompt(`Guess your favorite number (between 1 and 10). You have ${questionSixRemainingGuesses} attempts left.`));
+
+    if (questionSixGuess === favoriteNumber) {
+      alert(`Congratulations! ${favoriteNumber} is your favorite number!`);
       correctAnswerTotal++;
       break;
-    } else {
-      alert('Incorrect, try again!');
+    } else if (questionSixGuess< favoriteNumber) {
+      alert('Your guess is too low. Try again.');
+    } else if (questionSixGuess > favoriteNumber) {
+      alert('Your guess is too high. Try again.');
     }
-    questionSevenRemainingGuesses--;
+
+    questionSixRemainingGuesses--;
   }
 
-  if(questionSevenRemainingGuesses === 0){
-    alert(`Sorry, you've used all your attempts. The correct answers are ${questionSevenAnswers}.`);
+  if (questionSixRemainingGuesses === 0) {
+    alert(`Sorry, you've used all your attempts. The correct answer is ${favoriteNumber}.`);
   }
-
-  alert(`You got ${correctAnswerTotal} out of 7 questions correct!`);
 }
-questionSevenGuess();
+questionFavoriteNumber();
+
+function questionAnimals() {
+  function questionSevenGuess() {
+    let questionSevenAnswers = ['dog','rat','bunny'];
+    let questionSevenAllowedGuesses = 6;
+    let questionSevenRemainingGuesses = questionSevenAllowedGuesses;
+
+    for(let i=0; i < questionSevenAllowedGuesses; i++){
+      let questionSevenGuess = prompt(`Can you guess what animals I had as a kid? You have ${questionSevenRemainingGuesses} attempts left!`);
+
+      if (questionSevenAnswers.includes(questionSevenGuess)) {
+        let index = questionSevenAnswers.indexOf(questionSevenGuess);
+
+        let remainingAnswersString = '';
+
+        for (let i = 0; i < questionSevenAnswers.length; i++) {
+          if (i !== index) {
+            remainingAnswersString += questionSevenAnswers[i];
+            if (i < questionSevenAnswers.length - 2) {
+              remainingAnswersString += ' , ';
+            } else if (i === questionSevenAnswers.length - 2) {
+              remainingAnswersString += ' and ';
+            }
+          }
+        }
+        alert(`You are correct! ${questionSevenGuess} is one of the correct answers! The other correct answers are: ${remainingAnswersString}.`);
+
+        correctAnswerTotal++;
+        break;
+      } else {
+        alert('Incorrect, try again!');
+      }
+      questionSevenRemainingGuesses--;
+    }
+
+    if(questionSevenRemainingGuesses === 0){
+      alert(`Sorry, you've used all your attempts. The correct answers are ${questionSevenAnswers}.`);
+    }
+
+    alert(`You got ${correctAnswerTotal} out of 7 questions correct!`);
+  }
+  questionSevenGuess();
+}
+questionAnimals();
